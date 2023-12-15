e86f7b4c2187dbe93424cd3855408927091a1472
João Pedro Vitorio Santos Souza <132296221+danoninho2209@users.noreply.github.com>
2023-12-14 23:25:45 -0300

    Add files via upload

---
+++ clark.js
@@ -0,0 +1,48 @@
+document.getElementById('word').addEventListener('click', function() {
+    document.getElementById('sound').play();
+   });
+   document.getElementById('word1').addEventListener('click', function() {
+    document.getElementById('sound1').play();
+   });
+   document.getElementById('word2').addEventListener('click', function() {
+    document.getElementById('sound2').play();
+   });
+   document.getElementById('word3').addEventListener('click', function() {
+    document.getElementById('sound3').play();
+   });
+   document.getElementById('word4').addEventListener('click', function() {
+    document.getElementById('sound4').play();
+   });
+   document.getElementById('word5').addEventListener('click', function() {
+    document.getElementById('sound5').play();
+   });
+   document.getElementById('word6').addEventListener('click', function() {
+    document.getElementById('sound6').play();
+   });
+   document.getElementById('word7').addEventListener('click', function() {
+    document.getElementById('sound7').play();
+   });
+   document.getElementById('word8').addEventListener('click', function() {
+    document.getElementById('sound8').play();
+   });
+   document.getElementById('word9').addEventListener('click', function() {
+    document.getElementById('sound9').play();
+   });
+   document.getElementById('word10').addEventListener('click', function() {
+    document.getElementById('sound10').play();
+   });
+   document.getElementById('word11').addEventListener('click', function() {
+    document.getElementById('sound11').play();
+   });
+   document.getElementById('word12').addEventListener('click', function() {
+    document.getElementById('sound12').play();
+   });
+   document.getElementById('word13').addEventListener('click', function() {
+    document.getElementById('sound13').play();
+   });
+   document.getElementById('word14').addEventListener('click', function() {
+    document.getElementById('sound14').play();
+   });
+   document.getElementById('word15').addEventListener('click', function() {
+    document.getElementById('sound15').play();
+   });
